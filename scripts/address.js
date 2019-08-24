30fc14ae1b81c5f9def582a507fafc1d565a4887
FaustVX <jodu035@gmail.com>
2019-08-24 23:32:25 +0200

    Update index.js & index.html Added address.js

---
+++ scripts/address.js
@@ -0,0 +1,9 @@
+setAddresses = function(bar) {
+    const _55 = createLocalDistantUrl('192.168.1.55', 'box.faustvx.fr');
+    const box = createLocalDistantUrl('box.faustvx.fr');
+
+    bar.insertTab('domoticz', _55(':90/#/Login', false), 'Domoticz');
+    bar.insertTab('transmission', _55(':9091/transmission/web/', false), 'Transmission');
+    bar.insertTab('plex', _55(':32400/web/index.html#', false), 'Plex');
+    bar.insertTab('freebox', box(':6688/login.php', true), 'FreeBox');
+}
